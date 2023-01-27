c56f5629f3ec2b2cf1da10042cdba88cff7c9ee3
Brian Pfretzschner <info@brianp.de>
2023-01-27 21:05:35 +0100

    Update update_and_start.sh

--- update_and_start.sh
+++ update_and_start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+if [ ! -d server ]
+then
+    git clone https://github.com/knitter-org/server.git
+else
+    cd server && git pull && cd -
+fi
+
 docker-compose up -d --build --no-cache
